d0d0b937a5b1ddb9e92ff2d2d875daa1018cd352
BrachaBa <ba161977@gmail.com>
2024-07-30 23:30:51 +0300

    dist update

--- dist/ai-client/server/main.server.mjs
+++ dist/ai-client/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YRYRBA4S.mjs";import"./chunk-Y5SZ2VQK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-WOBFC3CY.mjs";import"./chunk-Y5SZ2VQK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
