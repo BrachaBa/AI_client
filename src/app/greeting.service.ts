9ac80ce526e3fb0aef0ac36a511fb08edf0f7c3b
BrachaBa <ba161977@gmail.com>
2024-09-26 18:36:48 +0300

    others

--- src/app/greeting.service.ts
+++ src/app/greeting.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GreetingService {
-  // private apiUrl = 'http://localhost:3000/generate-greeting';
-  private apiUrl = 'https://ai-server-k26q.onrender.com/generate-greeting';
+  private apiUrl = 'http://localhost:3000/generate-greeting';
+  // private apiUrl = 'https://ai-server-k26q.onrender.com/generate-greeting';
 
   constructor(private http: HttpClient) { }
 
